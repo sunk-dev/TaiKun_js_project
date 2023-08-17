19b22a452dfc039fd620bc6db98d97ce99ae0858
SeungHanLee <swq3064@naver.com>
2023-08-17 13:32:18 +0900

    요구사항 모달 틀 완성
    창을 띄우고 닫을 수 있음

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // 헤더에 요구사항 버튼
 const $demendBtn = document.getElementById("demend");
-console.log($demendBtn);
+// console.log($demendBtn);
 
 // 요구사항 모달엘리먼트
 const $demendModal = document.querySelector(".demend-modal");
-console.log($demendModal);
+// console.log($demendModal);
 
-const $demendModalCloseBtn = $demendModal.querySelector(".out-btn");
+// 닫기버튼
+const $demendModalCloseBtn = $demendModal.querySelector(".modal__actions");
 
 const CLASS_VISIBLE = "visible";
+const closeModal = (e) => {
+  $demendModal.classList.remove(CLASS_VISIBLE);
+};
 
 const showDemendModalHandler = (e) => {
   $demendModal.classList.add(CLASS_VISIBLE);
@@ -18,4 +22,4 @@ const showDemendModalHandler = (e) => {
 $demendBtn.addEventListener("click", showDemendModalHandler);
 
 // 요구사항 모달의 닫기 버튼을 눌렀을 때 닫는 기능
-$demendModalCloseBtn.addEventListener("click", close);
+$demendModalCloseBtn.addEventListener("click", closeModal);
