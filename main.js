acff28cff7a5b749d610204c1d2943b5fdf5cb0d
SeungHan Lee <swq3064@naver.com>
2023-08-17 15:26:32 +0900

    요구사항 모달 css 변경

--- main.js
+++ main.js
@@ -72,9 +72,8 @@ let milkValue = 0;
 milk.addEventListener("click", () => {
   console.log("젖소 버튼을 클릭했습니다");
   milkValue++;
-
   console.log("우유: ", milkValue);
-  $milk.textContent = `우유: 5/${aggValue}`;
+  $milk.textContent = `우유: 5/${milkValue}`;
 });
 const fleece = document.getElementById("ainmal3");
 let fleeceValue = 0;
@@ -82,7 +81,7 @@ fleece.addEventListener("click", () => {
   console.log("양 버튼을 클릭했습니다");
   fleeceValue++;
   console.log("양털: ", fleeceValue);
-  $fleece.textContent = `양털: 5/${aggValue}`;
+  $fleece.textContent = `양털: 5/${fleeceValue}`;
 });
 const duckEgg = document.getElementById("animal4");
 let duckEggValue = 0;
@@ -90,7 +89,7 @@ duckEgg.addEventListener("click", () => {
   console.log("오리 버튼을 클릭했습니다");
   duckEggValue++;
   console.log("오리알: ", duckEggValue);
-  $duckegg.textContent = `오리알: 5/${aggValue}`;
+  $duckegg.textContent = `오리알: 5/${duckEggValue}`;
 });
 const goatsMilk = document.getElementById("animal5");
 let goatsMilkValue = 0;
@@ -98,7 +97,7 @@ goatsMilk.addEventListener("click", () => {
   console.log("염소 버튼을 클릭했습니다");
   goatsMilkValue++;
   console.log("염소젖: ", goatsMilkValue);
-  $goatMilk.textContent = `염소젖: 5/${aggValue}`;
+  $goatMilk.textContent = `염소젖: 5/${goatsMilkValue}`;
 });
 const truffle = document.getElementById("animal6");
 let truffleValue = 0;
@@ -106,5 +105,5 @@ truffle.addEventListener("click", () => {
   console.log("돼지 버튼을 클릭했습니다");
   truffleValue++;
   console.log("트러플: ", truffleValue);
-  $truffle.textContent = `트러플: 5/${aggValue}`;
+  $truffle.textContent = `트러플: 5/${truffleValue}`;
 });
