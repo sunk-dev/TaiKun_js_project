913d57a8acd2c60604cb13588a4996f4706cf7ef
SeungHan Lee <swq3064@naver.com>
2023-08-18 13:45:31 +0900

    판매버튼과 요구사항버튼의 모달 분리(작업중)

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ const showMenuModalHandler = (e) => {
 $menuBtn.addEventListener("click", showMenuModalHandler);
 $exitBtn.addEventListener("click", exitMenuModalHandler);
 
-//요구사항 모달 구현(이승한
+// 요구사항 버튼
 // 헤더에 요구사항 버튼
-const $demendBtn = document.getElementById("sell");
+const $demendBtn = document.getElementById("demend");
 // console.log($demendBtn);
 
 // 요구사항 모달엘리먼트
@@ -35,12 +35,6 @@ const $demendModal = document.querySelector(".demend-modal");
 const $demendModalCloseBtn = $demendModal.querySelector(".modal__actions");
 // 요구사항 확인 컨텐츠들
 const $modal__content = document.querySelector(".modal__content");
-const $egg = $modal__content.querySelector(".egg");
-const $milk = $modal__content.querySelector(".milk");
-const $fleece = $modal__content.querySelector(".fleece");
-const $duckegg = $modal__content.querySelector(".duckegg");
-const $goatMilk = $modal__content.querySelector(".goatMilk");
-const $truffle = $modal__content.querySelector(".truffle");
 const CLASS_VISIBLE = "visible";
 
 const closeModal = (e) => {
@@ -57,6 +51,35 @@ $demendBtn.addEventListener("click", showDemendModalHandler);
 // 요구사항 모달의 닫기 버튼을 눌렀을 때 닫는 기능
 $demendModalCloseBtn.addEventListener("click", closeModal);
 
+// 판매 버튼
+const $sellBtn = document.getElementById("sell");
+
+const $sellModal = document.querySelector(".sell-modal");
+
+const $sellModalCloseBtn = $sellModal.querySelector(".sell_modal__actions");
+
+const $modal__content = document.querySelector(".sell_modal__content");
+const $egg = $modal__content.querySelector(".egg");
+const $milk = $modal__content.querySelector(".milk");
+const $fleece = $modal__content.querySelector(".fleece");
+const $duckegg = $modal__content.querySelector(".duckegg");
+const $goatMilk = $modal__content.querySelector(".goatMilk");
+const $truffle = $modal__content.querySelector(".truffle");
+const CLASS_VISIBLE = "visible";
+
+const closeModal = (e) => {
+  $sellModal.classList.remove(CLASS_VISIBLE);
+};
+
+const showDemendModalHandler = (e) => {
+  $sellModal.classList.add(CLASS_VISIBLE);
+};
+
+// 요구사항 버튼을 눌렀을 때 모달을 띄움
+$sellBtn.addEventListener("click", showDemendModalHandler);
+
+// 요구사항 모달의 닫기 버튼을 눌렀을 때 닫는 기능
+$sellModalCloseBtn.addEventListener("click", closeModal);
 
 //-------버튼 클릭 이벤트들(오영석)---------
 const agg = document.getElementById("animal1");
@@ -66,7 +89,6 @@ agg.addEventListener("click", () => {
   aggValue++;
   console.log("계란: ", aggValue);
   $egg.textContent = `계란: ${aggValue}/`;
-  
 });
 const milk = document.getElementById("animal2");
 let milkValue = 0;
@@ -111,10 +133,10 @@ truffle.addEventListener("click", () => {
 
 // 판매 버튼 이벤트
 const buyButton = document.getElementById("buy");
-buyButton.addEventListener('click', () => {
-  console.log('구매버튼');
+buyButton.addEventListener("click", () => {
+  console.log("구매버튼");
 });
 const sellButton = document.getElementById("sell");
-sellButton.addEventListener('click', () => {
-  console.log('판매버튼');
+sellButton.addEventListener("click", () => {
+  console.log("판매버튼");
 });
